3e349e99ecf813d7886d07078d0c1f65f4ba1a65
tahera2705 <taherasingaporewala@gmail.com>
2026-06-17 00:23:04 +0530

    Completed authentication module

--- app/api/user.py
+++ app/api/user.py
@@ -3,14 +3,26 @@ from sqlalchemy.orm import Session
 
 from app.db.dependencies import get_db
 from app.models.user import User
-from app.schemas.user import UserCreate, UserResponse
-from app.util.security import hash_password
+from app.schemas.user import (
+    UserCreate,
+    UserResponse,
+    UserLogin,
+    Token
+)
+from app.util.security import (
+    hash_password,
+    verify_password
+)
+from app.core.auth import create_access_token
 
 router = APIRouter(tags=["Users"])
 
 
 @router.post("/register", response_model=UserResponse)
-def register(user: UserCreate, db: Session = Depends(get_db)):
+def register(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
 
     existing_user = db.query(User).filter(
         User.email == user.email
@@ -33,3 +45,42 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+
+@router.post("/login", response_model=Token)
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+
+    db_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.hashed_password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    token_data = {
+        "sub": str(db_user.id)
+    }
+
+    access_token = create_access_token(
+        token_data
+    )
+
+    return Token(
+        access_token=access_token,
+        token_type="bearer"
+    )
---
+++ app/core/auth.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=30)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
